04689c723ed754b1d67856d0d98a1adbaf7a6271
Bharath Kumar P <117274872+7BharathKumar@users.noreply.github.com>
2025-03-14 18:34:19 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+'''boy_name = "ram"   #formated String
+girl_name = "sita"
+age_diff = abs(50-100)
+print(f"{boy_name} loves {girl_name}.Age Differnce {age_diff}")'''
+
+
+name = "Bharatha"   #indexing
+print(name[::2])  
+
