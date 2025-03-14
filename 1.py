b9144b85379193df25964424db8e097c191364b8
Bharath Kumar P <117274872+7BharathKumar@users.noreply.github.com>
2025-03-14 18:35:10 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,4 @@
-'''boy_name = "ram"   #formated String
+boy_name = "ram"   #formated String
 girl_name = "sita"
 age_diff = abs(50-100)
-print(f"{boy_name} loves {girl_name}.Age Differnce {age_diff}")'''
-
-
-name = "Bharatha"   #indexing
-print(name[::2])  
-
+print(f"{boy_name} loves {girl_name}.Age Differnce {age_diff}")
