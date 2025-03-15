77684ef2ee0f7870862c9d5b719a4d4e2d0292ef
Bharath Kumar P <117274872+7BharathKumar@users.noreply.github.com>
2025-03-15 14:49:21 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,18 @@
+items = ["apple", "Banana", "Orange"]   #Lists 
+print(items)
+
+items.append("Grapes")  #Append
+print(items)
+
+items.insert(1, "Mango")  #Insert
+print(items)
+
+items.remove("Banana")  #Remove
+print(items)
+
+items.pop()  #Pop
+print(items)
+
+
+items.clear()  #Clear
+print(items) 
